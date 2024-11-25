b49f8d2e575e4fcc217809f8c60dbf87b1004008
Richard-awesome <139611672+Richard-awesome@users.noreply.github.com>
2024-11-25 15:58:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let Likes = document.querySelector('.bi-hearts')
+let forYou = document.querySelector('#You')
+
+
+
+function Crush(){
+        Likes.classList.toggle('Likes')        
+}
+
+
+ Likes.addEventListener('click', Crush)
